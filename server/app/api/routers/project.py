844e51a27c8d5fcca94f3af1af24eacf18bf3000
강희진 <113979494+633jinn@users.noreply.github.com>
2025-05-19 16:12:59 +0900

    #10 Feat[be]: Refactor CRUD structure and add Keyword API (#12)
    * feat[be]: Add keyword API and Modify relationship to lazy join
    
    * feat[be]: Separate CRUD into Service and Repository And Add keyword API
    
    * fix[be]: Modify create_project return type

--- server/app/api/routers/project.py
+++ server/app/api/routers/project.py
@@ -1,25 +1,46 @@
-from fastapi import APIRouter, Depends, HTTPException
-from app.schemas.project import ProjectCreate, Project
-from sqlalchemy.orm import Session
-from app.infra.database.session import get_db
-from app.crud.project import create_project, get_project as get_projects
-from app.crud.user import get_user_by_email
+from fastapi import APIRouter, Depends
+from app.schemas.project import (
+    ProjectCreate,
+    Project,
+    ProjectKeywordsUpdate,
+    ProjectKeywordsBase,
+)
+from app.core.config.dependencies import get_project_service
+from app.services.project import ProjectService
 
 
 router = APIRouter()
 
-@router.post("/project")
-def create_projects(project: ProjectCreate, db: Session = Depends(get_db)):
-    db_user = get_user_by_email(db, email=project.user_email)
-    if not db_user:
-        raise HTTPException(status_code=400, detail="Can't find user")
-    
-    return create_project(db=db, project=project, user=db_user.id)
 
-@router.get("/project/{user_email}", response_model=list[Project])
-def get_project(user_email: str, db: Session = Depends(get_db)):
-    db_user = get_user_by_email(db, email=user_email)
-    if not db_user:
-        raise HTTPException(status_code=400, detail="Can't find user")
+@router.post("/project", response_model=Project)
+def create_projects(
+    project_dto: ProjectCreate, service: ProjectService = Depends(get_project_service)
+):
+    return service.create_project(project_dto=project_dto)
 
-    return get_projects(db=db, user=db_user.id)
+
+@router.get("/project", response_model=list[Project])
+def get_project(
+    user_email: str, service: ProjectService = Depends(get_project_service)
+):
+    return service.get_projects_by_user(user_email=user_email)
+
+
+@router.get("/project/{project_id}/keyword", response_model=ProjectKeywordsBase)
+def get_project_keyword(
+    project_id: int, service: ProjectService = Depends(get_project_service)
+):
+    return service.get_project_keywords(project_id=project_id)
+
+
+@router.patch("/{project_id}/keywords", response_model=ProjectKeywordsUpdate)
+def update_project_keyword(
+    project_id: int,
+    keywords_update: ProjectKeywordsUpdate,
+    service: ProjectService = Depends(get_project_service),
+):
+    # 키워드 업데이트
+    updated_project = service.update_project_keywords(
+        project_id=project_id, keywords_update=keywords_update
+    )
+    return updated_project
