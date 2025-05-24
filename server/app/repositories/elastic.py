bd54d89932fd100db5fa3a9d5ba83bf0f59b8bd5
JJadeYoon <yjs19990421@gmail.com>
2025-05-24 22:33:52 +0900

    feat[be]: Implement save log

--- server/app/repositories/elastic.py
+++ server/app/repositories/elastic.py
@@ -2,8 +2,10 @@ from fastapi import HTTPException
 from app.infra.database.elaticsearch import ElasticsearchClient
 from app.core.config.elastic_config import ELASTIC_MAPPINGS
 
-def save_log():
-    pass
+def save_log(index_name: str, log_data: dict):
+    """로그를 저장하는 함수"""
+    es = ElasticsearchClient()
+    es.index(index=index_name, body=log_data)
 
 def retrieve_log(index_name: str, query: str, category: str = None, start_time: str = None, end_time: str = None):
     """로그를 검색하는 함수"""
@@ -30,12 +32,3 @@ def retrieve_log(index_name: str, query: str, category: str = None, start_time: 
             detail="No logs found."
         )
     return search_by_hybrid
-
-def create_index(index_name: str):
-    es = ElasticsearchClient()
-    if es.indices.exists(index=index_name):
-        raise HTTPException(
-            status_code=500,
-            detail=f"Index {index_name} already exists."
-        )
-    es.indices.create(index=index_name, mappings=ELASTIC_MAPPINGS)
