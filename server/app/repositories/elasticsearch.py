e5a748e84820c84f2c33256371dc1aaace4832af
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2025-06-03 00:20:15 +0900

    #35 Feat[be]: Implement log search API endpoint  (#36)
    * feat[be]: Implement recent log API
    
    * fix[be]: Remove exception when get_logs_by_datetime is empty
    
    * feat[be]: Add log_level filter on elasticsearch repository and modify category to keyword
    
    * feat[be]: Implement get_logs_by_search endpoint
    
    ---------
    
    Co-authored-by: 633jinn <judyg00dy68@gmail.com>

--- server/app/repositories/elasticsearch.py
+++ server/app/repositories/elasticsearch.py
@@ -2,7 +2,7 @@ from fastapi import HTTPException
 from app.infra.database.elaticsearch import ElasticsearchClient
 from app.core.config.elastic_config import ELASTIC_MAPPINGS
 from typing import List, Dict, Any
-
+from app.core.enums.log_filter import LogLevelFilter
 
 es = ElasticsearchClient()
 
@@ -24,26 +24,33 @@ def save_log(index_name: str, log_data: dict):
     """로그를 저장하는 함수"""
     es.save_document(index=index_name, document=log_data)
 
-def retrieve_log(
+def retrieve_logs(
     index_name: str,
     query: str,
-    category: str = None,
+    keyword: str = None,
+    log_level: LogLevelFilter = None,
     start_time: str = None,
     end_time: str = None,
     k: int = 10,
 ):
     """로그를 검색하는 함수"""
-    category_filter = None
-    if category:
-        category_filter = {"category": category}
+    keyword_filter = None
+    if keyword:
+        keyword_filter = {"keyword": keyword}
+    log_level_filter = None
+    if log_level:
+        log_level_filter = {"log_level": log_level.value}
+    term_filter = [keyword_filter, log_level_filter]
+    
     time_filter = None
     if start_time and end_time:
         time_filter = {"message_timestamp": {"gte": start_time, "lte": end_time}}
+        
     search_by_hybrid = es.search_by_vector(
         index=index_name,
         query=query,
         filters=es.generate_filter(
-            term_filter=category_filter, range_filter=time_filter
+            term_filter=term_filter, range_filter=time_filter
         ),
         k=k,
     )
