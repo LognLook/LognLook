f08fe4d08435be0754dbbc2fc9a663fee3f0b273
JJadeYoon <yjs19990421@gmail.com>
2025-06-03 02:25:58 +0900

    fix[be]: Remove exceptions on empty elasticsearch results

--- server/app/repositories/elasticsearch.py
+++ server/app/repositories/elasticsearch.py
@@ -54,18 +54,12 @@ def retrieve_logs(
         ),
         k=k,
     )
-    if not search_by_hybrid:
-        raise HTTPException(status_code=404, detail="No logs found.")
     return search_by_hybrid
 
 def get_logs_by_ids(index_name: str, ids: List[str]) -> List[Dict[str, Any]]:
     """id로 로그를 검색하는 함수"""
     try:
         results = es.search_by_id(index=index_name, ids=ids)
-        if not results:
-            raise HTTPException(
-                status_code=404, detail="No logs found with the provided IDs."
-            )
         return results
     except HTTPException:
         raise
@@ -79,8 +73,6 @@ def get_logs_by_datetime(index_name: str, start_time: str, end_time: str):
     try:
         time_filter = {"message_timestamp": {"gte": start_time, "lte": end_time}}
         results = es.search_by_datetime(index=index_name, time_filter=time_filter)
-        # if not results:
-            # raise HTTPException(status_code=404, detail="No logs found with the provided datetime range.")
         return results
     except HTTPException:
         raise
